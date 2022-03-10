006d8ff98982d2d4ac02358d034841e8d66bea6a
Patrick Stanton <patrickstanton50@gmail.com>
2022-03-10 14:44:51 +0000

    project path

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,4 +9,9 @@ class Project extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'description'];
+
+    public function path()
+    {
+        return "/projects/{$this->id}";
+    }
 }
