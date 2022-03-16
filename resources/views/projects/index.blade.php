b8996e0cf56f89649f7ba333f4628fee70196d08
Patrick Stanton <patrickstanton50@gmail.com>
2022-03-16 16:03:01 +0000

    grid spacing with flex, project cards and nav logo

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,20 +1,26 @@
 @extends('layouts.app')
 
 @section('header')
-    <h1 class="text-green-800 font-bold text-5xl">Patboard</h1>
+    <div class="flex items-center justify-between">
+        <h2 class="text-slate-600 font-bold text-2xl text-opacity-80">My Projects</h1>
+        <div class="">
+            <a href="/projects/create" class="button">New Project</a>
+        </div>
+    </div>
 @endsection
 
 @section('body')
-    <div class="m-8">
-    <a href="projects/create" class="font-semibold text-indigo-600">Create New Project</a>
-    </div>
 
-    <div class="ml-10 flex">
+    <div class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
-            <div class="bg-white p-5 mr-4 rounded shadow w-1/3" style="height: 200px">
-                <h3 class="text-xl py-4">{{ $project->title }}</h3>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
+                    <h3 class="text-xl py-4 -ml-5 mb-3 border-l-4 border-slate pl-4">
+                        <a href="{{ $project->path() }}">{{ $project->title }}</a>
+                    </h3>
 
-                <div class="text-gray-400">{{ Illuminate\Support\Str::limit($project->description, 100) }}</div>
+                    <div class="text-gray-400">{{ Illuminate\Support\Str::limit($project->description, 100) }}</div>
+                </div>
             </div>
         @empty
             <div>No projects yet.</div>
